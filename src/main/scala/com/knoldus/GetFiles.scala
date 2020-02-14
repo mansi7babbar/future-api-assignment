b78c421fe86db9492bd7bb4d2049da395c798cbb
mansi7babbar <mansi7babbar@gmail.com>
2020-02-14 10:58:51 +0530

    updated scala assignment

--- src/main/scala/com/knoldus/GetFiles.scala
+++ src/main/scala/com/knoldus/GetFiles.scala
@@ -2,40 +2,34 @@ package com.knoldus
 
 import java.io.File
 
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 class GetFiles {
 
-  def getListOfFiles(dir: String): Future[ArrayBuffer[List[File]]] = Future {
-    val dirNames = ArrayBuffer[List[File]]()
+  def getListOfFiles(dir: String): Unit = {
     val d = new File(dir)
     val files = d.listFiles.toList
     for (file <- files) {
-      if (file.exists() && file.isDirectory && file.getName != "target") {
-        dirNames += file.listFiles.filter(_.isFile).toList
+      if (file.exists() && file.isDirectory) {
         getListOfFiles(file.getPath)
       }
       else {
-        ArrayBuffer[List[File]]()
+        println(file.getPath)
       }
-
     }
-    dirNames
   }
 
 }
 
 object GetFiles extends App {
   val getf = new GetFiles
-  val files = getf.getListOfFiles("/home/knoldus/IdeaProjects/future-api-assignment/src/main/scala/com/knoldus/my-folder")
-
-  files onComplete {
-    case Success(files) => println("List of files: " + files)
-    case Failure(exception) => println("Error: " + exception.getMessage)
+  val files = Future {
+    getf.getListOfFiles("/home/knoldus/IdeaProjects/future-api-assignment/src/main/scala/com/knoldus/my-folder")
   }
-
-  println("Ending...")
+  files onComplete {
+    case Success(files) => println(files)
+    case Failure(exception) => println("Error" + exception.getMessage)
+  }
 }
